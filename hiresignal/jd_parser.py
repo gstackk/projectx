48acb58756c892a5901fdbe60422eda69710079e
Gowtham R <93466485+gstackk@users.noreply.github.com>
2026-07-24 17:02:25 +0530

    Update jd_parser.py

--- hiresignal/jd_parser.py
+++ hiresignal/jd_parser.py
@@ -1,1 +1,10 @@
 
+from pydantic import BaseModel
+from typing import List
+
+
+class JDOutput(BaseModel):
+    required_skills: List[str]
+    nice_to_have: List[str]
+    years_exp: int
+    seniority: str
